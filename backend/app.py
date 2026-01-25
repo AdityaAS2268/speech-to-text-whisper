4f7200f046040b5d82e218fed0246c9acef5c935
AdityaAS2268 <aditya.ambaram@gmail.com>
2026-01-26 00:12:10 +0530

    Prepare app for development

--- backend/app.py
+++ backend/app.py
@@ -53,3 +53,7 @@ async def transcribe_audio(file: UploadFile = File(...)):
 @app.get("/")
 def root():
     return {"message": "FastAPI Whisper backend is running"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
